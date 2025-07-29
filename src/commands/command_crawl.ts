d74098cb217ae427c483e17fb7795bee2cf831da
if1live <libsora25@gmail.com>
2025-07-29 22:22:16 +0900

    오늘 기준으로 적절히 크롤링하는 명령어 추가

--- src/commands/command_crawl.ts
+++ src/commands/command_crawl.ts
@@ -32,7 +32,8 @@ const crawl = async (input: YearMonth) => {
   console.log(`Schedule data written to ${fp}`);
 };
 
-export const crawlCommand = new Command("crawl")
+// 지정된 날짜 크롤링
+export const crawlYearMonthCommand = new Command("crawl-year-month")
   .requiredOption("--list <strings...>", "yyyy-mm (예: 2025-01 2025-02)")
   .action(async (options) => {
     const list: YearMonth[] = options.list.map((item: string): YearMonth => {
@@ -45,3 +46,25 @@ export const crawlCommand = new Command("crawl")
     const tasks = list.map(async (x) => crawl(x));
     await Promise.all(tasks);
   });
+
+// 오늘을 기준으로 적절히 크롤링 범위 결정
+export const crawlFutureCommand = new Command("crawl-future").action(
+  async () => {
+    console.log("todo");
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = now.getMonth() + 1;
+
+    const targets: YearMonth[] = [];
+    for (let m = month; m <= month + 3; m++) {
+      const date = new Date(year, m - 1);
+      targets.push({
+        year: date.getFullYear(),
+        month: date.getMonth() + 1,
+      });
+    }
+
+    const tasks = targets.map(async (x) => crawl(x));
+    await Promise.all(tasks);
+  },
+);
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { Command } from "commander";
-import { crawlCommand } from "./commands/command_crawl.js";
+import {
+  crawlFutureCommand,
+  crawlYearMonthCommand,
+} from "./commands/command_crawl.js";
 import { icalCommand } from "./commands/command_ical.js";
 
 const program = new Command();
-program.addCommand(crawlCommand);
+program.addCommand(crawlYearMonthCommand);
+program.addCommand(crawlFutureCommand);
 program.addCommand(icalCommand);
 await program.parseAsync(process.argv);
